cd695d2029c766e0c2772d1275dd6fb3d11aa71e
EdoPan <81682326+EdoPan@users.noreply.github.com>
2024-04-28 15:57:01 +0200

    Add debug page

---
+++ AccessRoads/app/src/main/java/it/unipi/accessroads/DebugFragment.kt
@@ -0,0 +1,6 @@
+package it.unipi.accessroads
+
+import androidx.fragment.app.Fragment
+
+class DebugFragment : Fragment() {
+}
--- AccessRoads/app/src/main/java/it/unipi/accessroads/MainActivity.kt
+++ AccessRoads/app/src/main/java/it/unipi/accessroads/MainActivity.kt
@@ -1,14 +1,16 @@
 package it.unipi.accessroads
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import it.unipi.accessroads.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,17 +33,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        /*
+        val menuItem: MenuItem = menu.findItem(R.id.debug_page)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.MapFragment)
+        if (mapFragment != null) {
+            val check: Boolean = mapFragment.isVisible
+            menuItem.setVisible(check)
+        }
+        */
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        // Switching on the item id of the menu item
+        when (item.itemId) {
+            R.id.action_settings ->{
+                Toast.makeText(this, "Sensors", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.debug_page -> {
+                val navController = findNavController(R.id.nav_host_fragment_content_main)
+                navController.navigate(R.id.action_MapFragment_to_DebugFragment)
+                return true
+            }
         }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
