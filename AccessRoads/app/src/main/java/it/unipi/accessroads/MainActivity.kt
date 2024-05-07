a7809477ccd47cd83fc876fad2b4dea8a3329e22
Liu Chang <146169246+LiuChang01@users.noreply.github.com>
2024-05-07 17:51:12 +0200

    piu o meno  funziona ora

--- AccessRoads/app/src/main/java/it/unipi/accessroads/MainActivity.kt
+++ AccessRoads/app/src/main/java/it/unipi/accessroads/MainActivity.kt
@@ -1,12 +1,18 @@
 package it.unipi.accessroads
 
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import it.unipi.accessroads.databinding.ActivityMainBinding
 import it.unipi.accessroads.sensors.SemanticDetectionListener
 import it.unipi.accessroads.sensors.SemanticDetector
@@ -18,6 +24,7 @@ class MainActivity : AppCompatActivity(), SemanticDetectionListener {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var semanticDetector: SemanticDetector
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,16 +35,30 @@ class MainActivity : AppCompatActivity(), SemanticDetectionListener {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
+        fusedLocationProviderClient=LocationServices.getFusedLocationProviderClient(this)
         semanticDetector=SemanticDetector(this)
         semanticDetector.setSemanticDetectionListener(this)
         semanticDetector.startDetection()
     }
 
+    @SuppressLint("MissingPermission")
     override fun onSemanticEventDetected(semanticInfo: SemanticType) {
         Toast.makeText(this, "now pos$semanticInfo",Toast.LENGTH_SHORT).show()
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                if(location!=null){
+                    val latLng=LatLng(location.latitude,location.longitude)
+                    insertSemantic(latLng,semanticInfo)
+                }else{
+                }
+            }
     }
-
+    private fun insertSemantic(latLng: LatLng,semanticInfo: SemanticType) {
+        // Use the latLng value as needed
+        // For example, you can update UI elements, perform calculations, or trigger other actions
+        // Example:
+        Log.d("handleLocationUpdate", "Received location update - lat: ${latLng.latitude}, long: ${latLng.longitude} semtic ${semanticInfo}")
+    }
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
