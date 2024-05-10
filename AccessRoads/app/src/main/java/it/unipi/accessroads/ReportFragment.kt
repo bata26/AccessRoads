ed16f8260e05d5b2eef7431684f46dd08b92a7b7
Liu Chang <146169246+LiuChang01@users.noreply.github.com>
2024-05-11 00:16:29 +0200

    Update ReportFragment.kt

--- AccessRoads/app/src/main/java/it/unipi/accessroads/ReportFragment.kt
+++ AccessRoads/app/src/main/java/it/unipi/accessroads/ReportFragment.kt
@@ -1,18 +1,23 @@
 package it.unipi.accessroads
 
+import GPSManager
+import LocationResultListener
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.model.LatLng
 import it.unipi.accessroads.databinding.FragmentReportBinding
 import com.google.android.material.snackbar.Snackbar
+import it.unipi.accessroads.model.AccessibilityPoint
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class ReportFragment : Fragment() {
-
+    private lateinit var gpsManager: GPSManager
     private var _binding: FragmentReportBinding? = null
 
     // This property is only valid between onCreateView and
@@ -31,8 +36,9 @@ class ReportFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        gpsManager=GPSManager(requireContext())
         binding.reportButton.setOnClickListener {
+            sendReport()
             val snack = Snackbar.make(it,"Report submitted!",Snackbar.LENGTH_LONG)
             snack.show()
         }
@@ -42,4 +48,23 @@ class ReportFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+    private fun sendReport(){
+        gpsManager.getLastKnownLocation(object :LocationResultListener{
+            override fun onLocationResult(location: MutableMap<String, Double>) {
+                val latitude=location["lat"]
+                val longitude=location["long"]
+                if(latitude!=null&&longitude!=null){
+                    val latLng= LatLng(latitude, longitude)
+                    send(latLng)
+                }else{
+                    Log.d("error","Location is null")
+                }
+            }
+        })
+    }
+    private fun send(latLng: LatLng){
+        val accessibilityPoint=AccessibilityPoint(Db.getUUID(),latLng,1,"Stairs")
+        Db.postPoint(accessibilityPoint)
+    }
+
 }
