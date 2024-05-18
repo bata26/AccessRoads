79c2a66256a4bf2fd375ab1805238ade306bffba
Lorenzo Bataloni <batalonilorenzo@gmail.com>
2024-05-18 11:54:06 +0200

    remove comment

--- AccessRoads/app/src/main/java/it/unipi/accessroads/utils/BitmapHelper.kt
+++ AccessRoads/app/src/main/java/it/unipi/accessroads/utils/BitmapHelper.kt
@@ -12,10 +12,6 @@ import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 
 object BitmapHelper {
-    /**
-     * Demonstrates converting a [Drawable] to a [BitmapDescriptor], for use as a marker icon.
-     * Taken from ApiDemos on GitHub: https://github.com/googlemaps/android-samples/blob/master/ApiDemos/kotlin/app/src/main/java/com/example/kotlindemos/MarkerDemoActivity.kt
-     */
     fun vectorToBitmap(context: Context, @DrawableRes id: Int, @ColorInt color: Int): BitmapDescriptor {
         val vectorDrawable = ResourcesCompat.getDrawable(context.resources, id, null)
         if (vectorDrawable == null) {
